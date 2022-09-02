c5caab672d7a2a199e686d5616396c94b0febe78
Nicolas Dolinkue <dolinkue_n@hotmail.com>
2022-09-02 08:55:08 -0300

    start

--- HS13/ViewController.swift
+++ HS13/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    @IBAction func save(_ sender: UIButton) {
+    }
+    @IBAction func intensityChange(_ sender: Any) {
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
 
+        dismiss(animated: true)
+
+        currentImage = image
+    }
+    
 }
 
